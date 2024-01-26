d2409835ab1e35080c22306b179df9335ad01536
midasmsc <midasmsc@gmail.com>
2024-01-26 15:44:46 +0300

    Вроде всё

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/HelloController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/HelloController.java
@@ -10,10 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/")
     public String sayHello(ModelMap modelMap) {
-        List<String> messages = new ArrayList<>();
-        messages.add("Welcome on my page!");
-        messages.add("Click the button to authenticate and authorize!");
-        modelMap.addAttribute("messages", messages);
+//        List<String> messages = new ArrayList<>();
+//        messages.add("Welcome to the CRUD Spring Boot and Security testing!");
+//        messages.add("I hope it still works...");
+//        messages.add("Click the button to login!");
+//        modelMap.addAttribute("messages", messages);
         return "hello";
     }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -27,6 +27,8 @@ public class User implements UserDetails {
 
     @Column(name = "last_name")
     private String lastName;
+    @Column(name = "email")
+    private String email;
 
     @Column(name = "age")
     private Long age;
@@ -42,9 +44,12 @@ public class User implements UserDetails {
     public User() {
     }
 
-    public User(String name, String lastName, Long age) {
+    public User(Long id, String password, String name, String lastName, String email, Long age) {
+        this.id = id;
+        this.password = password;
         this.name = name;
         this.lastName = lastName;
+        this.email = email;
         this.age = age;
     }
 
@@ -126,17 +131,24 @@ public class User implements UserDetails {
         return roles;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(password, user.password) && Objects.equals(name, user.name) && Objects.equals(lastName, user.lastName) && Objects.equals(age, user.age) && Objects.equals(roles, user.roles);
+        return Objects.equals(id, user.id) && Objects.equals(password, user.password) && Objects.equals(name, user.name) && Objects.equals(lastName, user.lastName) && Objects.equals(email, user.email) && Objects.equals(age, user.age) && Objects.equals(roles, user.roles);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, password, name, lastName, age, roles);
+        return Objects.hash(id, password, name, lastName, email, age, roles);
     }
-
 }
